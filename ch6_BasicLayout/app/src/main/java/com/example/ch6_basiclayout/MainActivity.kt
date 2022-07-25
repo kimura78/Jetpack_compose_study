1745bbf0bfe0b4045ac98df809b83d06438855da
kimura <mkimura7878@gmail.com>
2022-07-25 12:19:49 +0900

    リストの利用を終了

--- ch6_BasicLayout/app/src/main/java/com/example/ch6_basiclayout/MainActivity.kt
+++ ch6_BasicLayout/app/src/main/java/com/example/ch6_basiclayout/MainActivity.kt
@@ -3,105 +3,93 @@ package com.example.ch6_basiclayout
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Bookmark
 import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import coil.compose.rememberImagePainter
 import com.example.ch6_basiclayout.ui.theme.Ch6_BasicLayoutTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Ch6_BasicLayoutTheme {
-               LayoutsCodelab()
+                ScrollingList()
             }
         }
     }
 }
 
 @Composable
-fun LayoutsCodelab() {
-    Scaffold(
-        topBar = {
-            TopAppBar(
-                title = {
-                    Text(text = "LayoutsCodelab")
-                },
-                actions = {
-                    IconButton(onClick = { /*TODO*/ }) {
-                        Icon(Icons.Filled.Bookmark, contentDescription = null)
-                    }
-                }
-            )
-        }
-    ) { innerPadding ->
-        BodyContent(Modifier.padding(innerPadding).padding(8.dp))
+fun ImageListItem(index: Int) {
+    Row(verticalAlignment = Alignment.CenterVertically) {
+        Image(
+            painter = rememberImagePainter(
+                data = "https://developer.android.com/images/brand/Android_Robot.png"
+            ),
+            contentDescription = "Android Logo",
+            modifier = Modifier.size(50.dp)
+        )
+        Spacer(Modifier.width(10.dp))
+        Text("Item #$index", style = MaterialTheme.typography.subtitle1)
     }
 }
 
 @Composable
-fun BodyContent(modifier: Modifier = Modifier) {
-    Column(modifier = modifier) {
-        Text(text = "Hi there!")
-        Text(text = "Thanks for going through the Layouts codelab")
+fun ScrollingList() {
+    val listSize = 100
+    val scrollState = rememberLazyListState()
+    val coroutineScope = rememberCoroutineScope()
+
+    Column {
+        Row {
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(0)
+                }
+            }) {
+                Text(text = "Scroll to the top")
+            }
+            Button(onClick = {
+                coroutineScope.launch {
+                    scrollState.animateScrollToItem(listSize - 1)
+                }
+            }) {
+                Text(text = "Scroll to the end")
+            }
+        }
+        LazyColumn(state = scrollState) {
+            items(100) {
+                ImageListItem(it)
+            }
+        }
     }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun LayoutsCodelabPreview() {
+fun LazyListPreview() {
     Ch6_BasicLayoutTheme {
-        LayoutsCodelab()
+        ScrollingList()
     }
 }
-
-//@Composable
-//fun PhotographerCard(modifier: Modifier = Modifier) {
-//    Row(modifier
-//        .padding(8.dp)
-//        .clip(RoundedCornerShape(4.dp))
-//        .clickable(onClick = {  })
-//        .padding(16.dp)
-//
-//    ) {
-//        Surface(
-//            modifier = Modifier.size(50.dp),
-//            shape = CircleShape,
-//            color = MaterialTheme.colors.onSurface.copy(alpha = 0.2f)
-//        ) {
-//
-//        }
-//        Column(
-//            modifier = Modifier
-//                .padding(start = 8.dp)
-//                .align(Alignment.CenterVertically)
-//        ) {
-//            Text("Alfred Sisley", fontWeight = FontWeight.Bold)
-//            CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
-//                Text("3 minutes ago", style = MaterialTheme.typography.body2)
-//            }
-//        }
-//    }
-//}
-//
-//@Preview(showBackground = true)
-//@Composable
-//fun PhotographerCardPreview() {
-//    Ch6_BasicLayoutTheme {
-//        PhotographerCard()
-//    }
-//}
-
